fcc72377fdcf18efacd110735aa7317cdbdf3f91
JStreamEC <105702681+JStreamEC@users.noreply.github.com>
2024-08-24 21:14:21 -0500

    Update vk.php

--- vk.php
+++ vk.php
@@ -1,13 +1,9 @@
 <?php
 function obtenerEnlaceM3U8($videoURL) {
-    // Obtén el contenido de la página del video
     $htmlContent = file_get_contents($videoURL);
-    
-    // Busca el enlace .m3u8 dentro del contenido
     $pattern = '/"hls_live":"(.*?)"/';
 
     if (preg_match($pattern, $htmlContent, $matches)) {
-        // Decodifica el enlace (ya que puede estar codificado con barras invertidas)
         $m3u8URL = stripslashes($matches[1]);
         return $m3u8URL;
     } else {
@@ -15,13 +11,17 @@ function obtenerEnlaceM3U8($videoURL) {
     }
 }
 
-// Ejemplo de uso
-$videoURL = "https://vk.com/video_ext.php?oid=764133947&id=456239397";
-$m3u8URL = obtenerEnlaceM3U8($videoURL);
+// Verificar si se pasa la URL como parámetro
+if (isset($_GET['url'])) {
+    $videoURL = $_GET['url'];
+    $m3u8URL = obtenerEnlaceM3U8($videoURL);
 
-if ($m3u8URL) {
-    echo "Enlace .m3u8 encontrado: " . $m3u8URL;
+    if ($m3u8URL) {
+        echo $m3u8URL;
+    } else {
+        echo "No se encontró el enlace .m3u8 en la página.";
+    }
 } else {
-    echo "No se encontró el enlace .m3u8 en la página.";
+    echo "No se proporcionó una URL.";
 }
 ?>
