102370a8a2695b6980fc8dfaa864465697d2cce0
JStreamEC <105702681+JStreamEC@users.noreply.github.com>
2024-08-24 21:17:46 -0500

    Update vk.php

--- vk.php
+++ vk.php
@@ -1,27 +1,23 @@
 <?php
-function obtenerEnlaceM3U8($videoURL) {
-    $htmlContent = file_get_contents($videoURL);
-    $pattern = '/"hls_live":"(.*?)"/';
-
-    if (preg_match($pattern, $htmlContent, $matches)) {
-        $m3u8URL = stripslashes($matches[1]);
-        return $m3u8URL;
-    } else {
-        return null;
-    }
+$filelink="https://vk.com/video_ext.php?oid=764133947&id=456239397";
+if ((strpos($filelink, 'vk.com') !== false) || (strpos($filelink, 'vkontakte.ru') !== false)) {
+  $ua="Mozilla/5.0 (Windows NT 10.0; rv:63.0) Gecko/20100101 Firefox/63.0";
+  $ch = curl_init($filelink);
+  curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+  curl_setopt($ch,CURLOPT_REFERER,"https://vk.com/");
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER  ,1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_HEADER,1);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+  $html = curl_exec($ch);
+  curl_close ($ch);
+  $html=str_replace("\\","",$html);
+  if (preg_match_all("/url\d+\":\"([http|https][\.\d\w\-\.\/\\\:\=\?\&\#\%\_\,]*)\"/",$html,$m))
+    $link=$m[1][count($m)-1];
+  else
+    $link="";
 }
-
-// Verificar si se pasa la URL como parámetro
-if (isset($_GET['url'])) {
-    $videoURL = $_GET['url'];
-    $m3u8URL = obtenerEnlaceM3U8($videoURL);
-
-    if ($m3u8URL) {
-        echo $m3u8URL;
-    } else {
-        echo "No se encontró el enlace .m3u8 en la página.";
-    }
-} else {
-    echo "No se proporcionó una URL.";
-}
+echo $link;
 ?>
