ec4445b5e2d29109e8bcb04b970cbefc72784616
JStreamEC <105702681+JStreamEC@users.noreply.github.com>
2025-10-29 19:00:25 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 [
   {
-    "daddy_base": "https://newzar.xyz/premiumtv/daddyhd.php?id=",
+    "daddy_base": "https://truncatedactivitiplay.xyz/premiumtv/daddyhd.php?id=",
     "auth_base": "https://top2new.newkso.ru/auth.php",
     "referer": "https://jxoplay.xyz/",
     "origin": "https://jxoplay.xyz/"
